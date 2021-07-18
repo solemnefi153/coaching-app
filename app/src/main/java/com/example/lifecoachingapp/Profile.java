1ed66343a492eb2b1328c1379383dd68a566bc45
Nefi Aguilar <solemnefi153@gmail.com>
2021-07-18 05:30:23 -0600

    Add logic to profile page

--- app/src/main/java/com/example/lifecoachingapp/Profile.java
+++ app/src/main/java/com/example/lifecoachingapp/Profile.java
@@ -3,20 +3,64 @@ package com.example.lifecoachingapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Profile extends AppCompatActivity {
 
+    //UI Elements
     BottomNavigationView bottom_nav;
+    TextInputEditText username_input;
+    ImageButton saveUsername;
+
+    //Necessary data for the UI
+    SharedPreferences sp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Grab UI elements
+        username_input = findViewById(R.id.username_input);
+        saveUsername = findViewById(R.id.user_name_edit_btn);
+
+        //Initialize the shared preferences
+        //A list of chapters with its respective progress saved in the shared preferences
+        sp = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        if(sp.contains("username")){
+            String username = sp.getString("username", "");
+            username_input.setText(username);
+        }
+
+        //Add an event listener to the save btn
+        saveUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Grab the current value of the input
+                String newUsername = username_input.getText().toString().trim();
+                SharedPreferences.Editor editor = sp.edit();
+                //Increment the value saved in this preference
+                editor.putString("username",  newUsername);
+                editor.apply();
+                //Show a toast confirming the username update
+                Toast.makeText(getApplicationContext(), "Saved username as: " + newUsername, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
 
         //Initialize the bottom nav and set an event listener to the the bottom nav menu
         bottom_nav = findViewById(R.id.bottom_nav);
